c20a4724785bdcbc5be88511bfcf0b9005476a22
Ahmad <ahmdaf420@gmail.com>
2023-01-01 18:46:15 +0200

    firebase-log in/sign up working ,with some proplems in routing

--- be-smart-frontend/src/App.js
+++ be-smart-frontend/src/App.js
@@ -1,41 +1,52 @@
-import "./App.css";
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
-import SiginUp from "./pages/SiginUp";
+import React from "react";
+import Signup from "./pages/SiginUp";
+import { Container, Navbar } from "react-bootstrap";
 import { AuthProvider } from "./contexts/AuthContext";
-
-// import Footer from "./components/Footer";
-// import NavBar from "./components/NavBar";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Profile";
+import Login from "./pages/Login";
+import PrivateRoute from "./pages/PrivateRoute";
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdateProfile from "./pages/UpdateProfile";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <AuthProvider>
-        <NavBar />
-        <Home />
-        <Footer />
-        <SiginUp />
-      </AuthProvider>
-    </div>
-   )}
+    <>
+      <NavBar />
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{}}>
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/profile" element={<PrivateRoute />}>
+                  {/* <Route exact path="/profile" element={<Dashboard />} /> */}
+                </Route>
 
+                <Route exact path="/update-profile" element={<PrivateRoute />}>
+                  {/* <Route
+                    exact
+                    path="/update-profile"
+                    element={<UpdateProfile />}
+                  /> */}
+                </Route>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+      <Footer />
+    </>
+  );
+}
 
 export default App;
-
-
-    /* <NavBar />
-    <BrowserRouter basename={"/"}>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<SiginUp />} />
-      </Routes>
-    </BrowserRouter>
-
-    <Footer /> */
-  
-  
-
--- be-smart-frontend/src/utils/firebaseConfig.js
+++ be-smart-frontend/src/utils/firebaseConfig.js
@@ -1,8 +1,8 @@
-// Import the functions you need from the SDKs you need
-import "firebase/compat/auth";
-import firebase from "firebase/compat/app";
+import firebase from "firebase/app";
+import "firebase/auth";
+
 const app = firebase.initializeApp({
-  apiKey: "asdf",
+  apiKey: "2134",
   authDomain: "",
   projectId: "",
   storageBucket: "",
