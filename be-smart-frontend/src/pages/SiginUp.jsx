08f83bed7d7d611b2631f94ba9eacb1a03da0071
Omar Sobh <98892376+OmarSobh@users.noreply.github.com>
2023-01-03 12:35:23 +0200

    Sign in And Sign Up Mdb Desgin

--- be-smart-frontend/src/pages/SiginUp.jsx
+++ be-smart-frontend/src/pages/SiginUp.jsx
@@ -3,7 +3,21 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Signup() {
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBInput,
+  MDBIcon,
+  MDBCheckbox
+}
+  from 'mdb-react-ui-kit';
+
+function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
@@ -18,6 +32,9 @@ export default function Signup() {
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match");
     }
+    // else if (regex){
+
+    // }
 
     try {
       setError("");
@@ -32,33 +49,76 @@ export default function Signup() {
   }
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+    <MDBContainer fluid>
+
+      <MDBCard className='text-black m-5' style={{ borderRadius: '25px' }}>
+        <MDBCardBody>
           {error && <Alert variant="danger">{error}</Alert>}
+
           <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
+            <MDBRow >
+              <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+
+                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+
+
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="envelope me-3" size='lg' />
+                  <MDBInput label='Your Email' id='form2' type='email' ref={emailRef} required />
+                </div>
+
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="lock me-3" size='lg' />
+                  <MDBInput label='Password' id='form3' type='password' ref={passwordRef} required />
+                </div>
+
+                <div className="d-flex flex-row align-items-center mb-4">
+                  <MDBIcon fas icon="key me-3" size='lg' />
+                  <MDBInput label='Repeat your password' id='form4' type='password' ref={passwordConfirmRef} required />
+                </div>
+
+                <div className='mb-4'>
+                  <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' /> 
+                  
+
+                </div>
+
+                <MDBBtn className='mb-4' size='lg' disabled={loading} type="submit">Register</MDBBtn>
+                {/* <div className="mb-4">
+                  Already have an account? <Link to="/login">Log In</Link>
+
+                </div> */}
+              </MDBCol>
+
+              <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+                <MDBCardImage src={require('../Images/jpeg.jpeg')} fluid />
+              </MDBCol>
+
+            </MDBRow>
+            <div className="w-100 text-center mt-2">
+              Already have an account? <Link to="/login">Log In</Link>
+            </div>
           </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
-      </div>
-    </>
+        </MDBCardBody>
+      </MDBCard>
+
+
+
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+        rel="stylesheet"
+      />
+
+    </MDBContainer>
   );
 }
+
+export default Signup;
+
+
+
--- be-smart-frontend/src/utils/firebaseConfig.js
+++ be-smart-frontend/src/utils/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "2134",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: process.env.REACT_APP_PUBLIC_API_KEY,
+  authDomain: process.env.REACT_APP_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_PUBLIC_APP_ID,
+  measurementId: process.env.REACT_APP_PUBLIC_MEASUREMENT_ID,
 });
 
 export const auth = app.auth();
