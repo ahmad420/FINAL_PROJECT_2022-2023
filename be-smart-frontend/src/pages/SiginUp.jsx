d44e23e19f6a39e37e4a0fd1580c300d09de6cea
Omar Sobh <98892376+OmarSobh@users.noreply.github.com>
2023-01-09 16:40:57 +0200

    sign in and up background img update

--- be-smart-frontend/src/pages/SiginUp.jsx
+++ be-smart-frontend/src/pages/SiginUp.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
+import "../style/SiginUp-In.css"
 
 import {
   MDBBtn,
