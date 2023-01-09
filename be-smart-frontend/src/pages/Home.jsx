a93fbd1b41006cf85fe8c69bd8cb815112dd6bd4
Omar Sobh <98892376+OmarSobh@users.noreply.github.com>
2023-01-09 14:11:36 +0200

    about section added

--- be-smart-frontend/src/pages/Home.jsx
+++ be-smart-frontend/src/pages/Home.jsx
@@ -30,6 +30,37 @@ const Home = () => {
 
           </div>
         </div>
+        <div className="about">
+          <div className="right">
+          <div className="section1">
+            <div className="section1Titel">
+              <h2>
+                Industries We Servicse
+              </h2>
+            </div>
+            <div className="section1Text">
+              <p>
+                Our Warehouse Management System can be remotely deployed in less than 7 days.
+                </p>
+            </div>
+          </div>
+          </div>
+          <div className="left">
+          <div className="section2">
+            <div className="section1Titel">
+              <h2>
+                Industries We Servicse
+              </h2>
+            </div>
+            <div className="section2Text">
+              <p>
+                Our Warehouse Management System can be remotely deployed in less than 7 days.
+                </p>
+            </div>
+          </div>
+          </div>
+
+        </div>
         <div className="auction">
           <div className="title">
             <p className="titlebold">Be Smart Servicse</p>
