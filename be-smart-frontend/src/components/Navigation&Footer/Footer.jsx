0a98f0bcaaf24f976419fd54d30fe417e5318d68
Ahmad <ahmdaf420@gmail.com>
2023-01-09 14:29:29 +0200

    changed footer

--- be-smart-frontend/src/components/Navigation&Footer/Footer.jsx
+++ be-smart-frontend/src/components/Navigation&Footer/Footer.jsx
@@ -1,83 +1,134 @@
-import React from 'react';
-import "./Footer.css"
+import React from "react";
 import {
   MDBFooter,
   MDBContainer,
-  MDBCol,
   MDBRow,
+  MDBCol,
   MDBIcon,
-  MDBBtn
-} from 'mdb-react-ui-kit';
+} from "mdb-react-ui-kit";
 
-export default function Footer() {
+export default function App() {
   return (
-    <MDBFooter id='footer' className='bg-dark" text-center text-dark '>
-      <MDBContainer className='p-4 pb-0'>
-        <section className='mb-4'>
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#3b5998' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='facebook-f' />
-          </MDBBtn>
+    <MDBFooter bgColor="light" className="text-center text-lg-start text-muted">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#55acee' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='twitter' />
-          </MDBBtn>
+        <div>
+          <a href="/" className="me-4 text-reset">
+            <MDBIcon fab icon="facebook-f" />
+          </a>
+          <a href="/" className="me-4 text-reset">
+            <MDBIcon fab icon="twitter" />
+          </a>
+          <a href="/" className="me-4 text-reset">
+            <MDBIcon fab icon="google" />
+          </a>
+          <a href="/" className="me-4 text-reset">
+            <MDBIcon fab icon="instagram" />
+          </a>
+          <a href="/" className="me-4 text-reset">
+            <MDBIcon fab icon="linkedin" />
+          </a>
+          <a href="/" className="me-4 text-reset">
+            <MDBIcon fab icon="github" />
+          </a>
+        </div>
+      </section>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#dd4b39' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='google' />
-          </MDBBtn>
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#ac2bac' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='instagram' />
-          </MDBBtn>
+      <section className="">
+        <MDBContainer className="text-center text-md-start mt-5">
+          <MDBRow className="mt-3">
+            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                <MDBIcon icon="gem" className="me-3" />
+                Company name
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer
+                content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit.
+              </p>
+            </MDBCol>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#0082ca' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='linkedin-in' />
-          </MDBBtn>
+            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Angular
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  React
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Vue
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Laravel
+                </a>
+              </p>
+            </MDBCol>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#333333' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='github' />
-          </MDBBtn>
-        </section>
-      </MDBContainer>
+            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Help
+                </a>
+              </p>
+            </MDBCol>
 
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright : BeSmart
-       
+            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+              <p>
+                <MDBIcon icon="home" className="me-2" />
+                New York, NY 10012, US
+              </p>
+              <p>
+                <MDBIcon icon="envelope" className="me-3" />
+                info@example.com
+              </p>
+              <p>
+                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
+              </p>
+              <p>
+                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © 2021 Copyright:
+        <a className="text-reset fw-bold" href="/">
+          MDBootstrap.com
+        </a>
       </div>
     </MDBFooter>
   );
